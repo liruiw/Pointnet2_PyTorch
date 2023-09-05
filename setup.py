e63a98d67faab810988200031abd15c889c3741d
Lirui Wang (Leroy) <1003301648@qq.com>
2023-09-05 08:23:06 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os.path as osp
 
 from setuptools import find_packages, setup
 
-requirements = ["hydra-core", "pytorch-lightning"]
+requirements = ["hydra-core", "pytorch-lightning", "numpy", "msgpack-numpy""lmdb","h5py","ninja","hydra-core","./pointnet2_ops_lib/."]
 
 
 exec(open(osp.join("pointnet2", "_version.py")).read())
